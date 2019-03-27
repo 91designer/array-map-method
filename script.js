7540152c195f254cac57b4218a9ccff4976f69e8
mohamed morh <mc.marah.91@gmail.com>
2019-03-27 13:25:07 +0000

    create file 1
    map method

---
+++ script.js
@@ -0,0 +1,12 @@
+let nums = [1,5,45,12,65];
+let multipled   = nums.map(num => num*2);
+let devided     = nums.map(num => num/3);
+let substructed = nums.map(num => num-5);
+let added       = nums.map(num => num+1020);
+
+console.clear()
+console.log(nums)
+console.log(multipled)
+console.log(devided)
+console.log(substructed)
+console.log(added)
